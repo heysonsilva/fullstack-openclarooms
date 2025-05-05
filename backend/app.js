0885aa8be6bbb34af7ef9131c63910988a09aabd
Heyson Silva <116231349+heysonsilva@users.noreply.github.com>
2025-05-05 00:05:16 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,21 +3,8 @@ import express from "express";
 const app = express()
 app.use(express.json())
 
-app.use((req, res, next)=>{
-    console.log("Requisição Recebida com Sucesso !!")
-    next()
-})
-
-app.use((req, res, next)=>{
-    const STATUSCODE = 200
-    res.status(STATUSCODE)
-    console.log("STATUS CODE ALTERADO COM SUCESSO !!")
-    next()
-})
-
 app.use((req, res)=>{
     res.send({'message':'my first server app'})
-    console.log("resposta enviada com sucesso !!")
 })
 
 export default app;
